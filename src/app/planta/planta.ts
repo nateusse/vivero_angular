cf0cea4a550ea162bb861415d67c0f3cf7388aad
nateusse <natyeusse@gmail.com>
2025-04-27 10:15:45 -0400

    refactor: change interface Planta for class

--- src/app/planta/planta.ts
+++ src/app/planta/planta.ts
@@ -1,9 +1,27 @@
-export interface Planta {
-    id: number;
-    nombre_comun: string;
-    nombre_cientifico: string;
-    tipo: string;
-    clima: string;
-    imagen: string;
-    descripcion: string;
+export class Planta {
+  id: number;
+  nombre_comun: string;
+  nombre_cientifico: string;
+  tipo: string;
+  clima: string;
+  imagen: string;
+  descripcion: string;
+
+  constructor(
+    id: number,
+    nombre_comun: string,
+    nombre_cientifico: string,
+    tipo: string,
+    clima: string,
+    imagen: string,
+    descripcion: string
+  ) {
+    this.id = id;
+    this.nombre_comun = nombre_comun;
+    this.nombre_cientifico = nombre_cientifico;
+    this.tipo = tipo;
+    this.clima = clima;
+    this.imagen = imagen;
+    this.descripcion = descripcion;
   }
+}
